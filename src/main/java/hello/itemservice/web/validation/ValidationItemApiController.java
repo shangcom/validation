55f3a6a6fdb95b756455d0c657699ad0d0a6505a
shangcom <dndbs91@gmail.com>
2024-10-04 21:09:27 +0900

    검증2 - Bean Validation
    HTTP 메시지 컨버터
    
    진행중

---
+++ src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -0,0 +1,32 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+    /*
+    만약 postman을 사용해서 json 형식으로 "price":"AAA"로 요청한다면
+     */
+    @PostMapping("/add")
+    public Object addItem(@Validated @RequestBody ItemSaveForm form, BindingResult bindingResult) {
+        log.info("API 컨트롤러 호출");
+
+        if (bindingResult.hasErrors()) {
+            log.info("검증 오류 발생= {}", bindingResult);
+            return bindingResult.getAllErrors();
+        }
+
+        log.info("성공 로직 실행");
+        return form;
+    }
+}
